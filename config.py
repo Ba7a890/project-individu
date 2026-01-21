36f27a5e2dbabd7531291659364311495efdc708
Ba7a890 <teddyeka.tambunan@gmail.com>
2026-01-21 20:40:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-TOKEN = "MTM4NDg4Nzk5MDg5MDc5NTEzOA.GiIy6m.VozUVofN2pVLTZzxiPN5Z2K93cgwecACeyw0IM"
+TOKEN = "YOUR TOKEN🤗"
 PREFIX = "!"
 
 # ===== BOT SETTING =====
 BOT_NAME = "Movie Recommendation Bot"
+
 EMBED_COLOR = 0x2ecc71  # hijau
